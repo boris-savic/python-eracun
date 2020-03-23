d262b5645a2e2bf59bfd98eeb3f161c7d21c9ddb
Boris Savic <boris@olaii.com>
2020-03-23 10:58:15 +0100

    - prepare 0.2.0 release

--- setup.py
+++ setup.py
@@ -3,12 +3,12 @@ from setuptools import setup, find_packages
 setup(
     name='eracun_generator',
     packages=['eracun_generator', 'eracun_generator.envelope'],
-    version='0.1.8',
-    description='e-SLOG e-Racun v1.6.1 XML generator',
+    version='0.2.0',
+    description='e-SLOG e-Racun v1.6.1 and v2.0 XML generator',
     author='Boris Savic',
     author_email='boris70@gmail.com',
     url='https://github.com/boris-savic/python-eracun',
-    download_url='https://github.com/boris-savic/python-eracun/tarball/0.1.8',
+    download_url='https://github.com/boris-savic/python-eracun/tarball/0.2.0',
     keywords=['python eracun', 'e-racun', 'e-slog'],
     classifiers=[],
     install_requires=[
